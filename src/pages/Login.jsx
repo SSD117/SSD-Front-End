ac3b3c69a2e64266c49358ba65595c5e2ca1a196
SeongYun_Cheon <1112csy@naver.com>
2024-11-20 21:43:46 +0900

    chore: 로그인페이지 디자인 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,42 +10,47 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100 p-6">
-      <img src={AppLogo} alt="App Logo" className="w-3/5 h-auto my-6 object-contain" />
-      <label htmlFor="email" className="text-sm text-gray-600 w-full max-w-md mb-2">
-        이메일
-      </label>
-      <input
-        type="email"
-        id="email"
-        placeholder="이메일을 입력하세요"
-        required
-        className="p-3 mb-4 w-full max-w-md text-sm border border-gray-300 rounded focus:outline-none focus:border-main01"
-      />
+    <div className="flex flex-col items-center min-h-screen bg-white p-6">
+  <div className="bg-white w-full max-w-md p-6 rounded-lg shadow-md mb-6 flex flex-col items-center">
+    <img src={AppLogo} alt="App Logo" className="w-2/3 h-auto mb-6 object-contain" />
+    
+    <label htmlFor="email" className="text-sm text-gray-600 w-full mb-2 block">
+      이메일
+    </label>
+    <input
+      type="email"
+      id="email"
+      placeholder="이메일을 입력하세요"
+      required
+      className="p-3 mb-4 w-full text-sm border border-gray-300 rounded focus:outline-none focus:border-main01"
+    />
 
-      <label htmlFor="password" className="text-sm text-gray-600 w-full max-w-md mb-2">
-        비밀번호
-      </label>
-      <input
-        type="password"
-        id="password"
-        placeholder="비밀번호를 입력하세요"
-        required
-        className="p-3 mb-4 w-full max-w-md text-sm border border-gray-300 rounded focus:outline-none focus:border-main01"
-      />
+    <label htmlFor="password" className="text-sm text-gray-600 w-full mb-2 block">
+      비밀번호
+    </label>
+    <input
+      type="password"
+      id="password"
+      placeholder="비밀번호를 입력하세요"
+      required
+      className="p-3 mb-4 w-full text-sm border border-gray-300 rounded focus:outline-none focus:border-main01"
+    />
+  </div>
 
-      <button
-        type="submit"
-        className="p-3 mb-4 w-full max-w-md text-lg text-white bg-main01 border border-main01 rounded cursor-pointer"
-      >
-        로그인
-      </button>
-      <button
-        type="button"
-        onClick={handleSignupClick}
-        className="p-3 w-full max-w-md text-lg text-main01 bg-white border border-main01 rounded cursor-pointer">
-        회원가입
-      </button>
-    </div>
+  <button
+    type="submit"
+    className="p-3 mb-4 w-full max-w-md text-lg text-white bg-main01 border border-main01 rounded shadow-md hover:shadow-lg transition-shadow"
+  >
+    로그인
+  </button>
+  <button
+    type="button"
+    onClick={handleSignupClick}
+    className="p-3 w-full max-w-md text-lg text-main01 bg-white border border-main01 rounded shadow-md hover:shadow-lg transition-shadow"
+  >
+    회원가입
+  </button>
+</div>
+
   );
 }
